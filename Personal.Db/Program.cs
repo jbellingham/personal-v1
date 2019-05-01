c06cabb78baa7872f7116891d9a3c35c7a2d64a4
Jesse Bellingham <jesse.bellingham@acurus.com.au>
2019-05-01 22:32:22 +1000

    feat(docker): building out docker containers for each chunk of the proj

---
+++ Personal.Db/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Personal.Domain;
+
+namespace Personal.Db
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataContextFactory = new DataContextFactory();
+            var dataContext = dataContextFactory.CreateDbContext(new string[] {});
+//            var dataSeeder = new DataSeeder(dataContextFactory.Container, dataContext);
+
+            if (args.Any(_ => _ == "--migrate"))
+            {
+                Console.WriteLine("Applying migrations");
+                dataContext.Database.Migrate();
+            }
+//            else
+//            {
+//                if (args.Length > 0)
+//                {
+//                    foreach (var seedClassname in args)
+//                    {
+//                        dataSeeder.ApplySeedData(seedClassname);
+//                    }
+//                }
+//                else
+//                {
+//                    dataSeeder.ApplySeedData();
+//                }
+//            }
+        }
+    }
+}
